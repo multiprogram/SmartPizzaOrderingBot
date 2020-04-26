63554dcb79c1652384d1af5eac4520841eee3f28
multiprogram <126498341+multiprogram@users.noreply.github.com>
2020-04-26 10:05:38 +0000

    wordsmith

--- pizzawit.py
+++ pizzawit.py
@@ -1,2 +1,3 @@
 # pizzawit - Talks to wit.ai for speech recognition and analysis in order
-# to make a pizza-ordering chatbot. No actual phone call i
+# to make a pizza-ordering chatbot. No actual phone call is made.
+#
