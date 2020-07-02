629784c99d1e54a53d3ffc168f1b38a304c3fd20
multiprogram <126498341+multiprogram@users.noreply.github.com>
2020-07-02 17:46:05 +0000

    Update local.py

--- pizzawit.py
+++ pizzawit.py
@@ -7,4 +7,5 @@
 #
 # Permission is hereby granted, free of charge, to any person obtaining 
 # a copy of this software and associated documentation files (the 
-# "Software"), to deal in the Software without restriction, inc
+# "Software"), to deal in the Software without restriction, including 
+#
