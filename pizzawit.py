4ef888e7294dc245872d6e728f40156bb78c057c
multiprogram <126498341+multiprogram@users.noreply.github.com>
2020-07-30 06:53:38 +0000

    Prepare 2.3.0 release

--- pizzawit.py
+++ pizzawit.py
@@ -9,4 +9,4 @@
 # a copy of this software and associated documentation files (the 
 # "Software"), to deal in the Software without restriction, including 
 # without limitation the rights to use, copy, modify, merge, publish, 
-# di
+# distribute, subl
