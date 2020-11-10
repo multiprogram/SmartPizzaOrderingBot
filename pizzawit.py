f2321f62ebb581b62125776a9de4236955030f86
multiprogram <126498341+multiprogram@users.noreply.github.com>
2020-11-10 15:58:03 +0000

    update FINISHED ; MANIFEST status

--- pizzawit.py
+++ pizzawit.py
@@ -10,4 +10,5 @@
 # "Software"), to deal in the Software without restriction, including 
 # without limitation the rights to use, copy, modify, merge, publish, 
 # distribute, sublicense, and/or sell copies of the Software, and to 
-# permit persons to whom the Software is furnished to do so,
+# permit persons to whom the Software is furnished to do so, subject 
+#
