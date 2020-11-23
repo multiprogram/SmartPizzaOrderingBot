39f0d966a52f7b93a0c8fe7acf510e34eb3c6e87
multiprogram <126498341+multiprogram@users.noreply.github.com>
2020-11-23 06:12:09 +0000

    Update ImageManager.py

--- pizzawit.py
+++ pizzawit.py
@@ -11,4 +11,5 @@
 # without limitation the rights to use, copy, modify, merge, publish, 
 # distribute, sublicense, and/or sell copies of the Software, and to 
 # permit persons to whom the Software is furnished to do so, subject 
-# to the following cond
+# to the following conditions:
+#
