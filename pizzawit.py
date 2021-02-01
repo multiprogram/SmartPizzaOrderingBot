41a28317bd8807fe329815703f347372ae5080f8
multiprogram <126498341+multiprogram@users.noreply.github.com>
2021-02-01 05:37:37 +0000

    chore: updated .travis.yml

--- pizzawit.py
+++ pizzawit.py
@@ -14,4 +14,6 @@
 # to the following conditions:
 #
 # The above copyright notice and this permission notice shall be 
-# included in all copies or substantial portions of the So
+# included in all copies or substantial portions of the Software.
+#
+# THE S
