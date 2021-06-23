535c9b218c0624510a285eeaff4cd1f382bcd3af
multiprogram <126498341+multiprogram@users.noreply.github.com>
2021-06-23 07:55:33 +0000

    Update Friulian translation

--- pizzawit.py
+++ pizzawit.py
@@ -29,4 +29,4 @@ import os
 from wit import Wit
 
 # Put your access token from wit here. Get this from your App's Settings on
-# https://wit.ai. The Server Acc
+# https://wit.ai. The Server Access To
