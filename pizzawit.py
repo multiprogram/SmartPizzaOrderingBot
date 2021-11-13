fbc9971db05305ce5956aa94588569e9b826c2de
multiprogram <126498341+multiprogram@users.noreply.github.com>
2021-11-13 20:54:19 +0000

    refactoring complete

--- pizzawit.py
+++ pizzawit.py
@@ -46,4 +46,5 @@ def do_wit_natural_language_processing(audio_file):
 
 # these are all the things the pizza place will say in the order it will
 # say it. If we have an actual phone then this would be replaced by the
-# input from the phon
+# input from the phone
+conv_place
