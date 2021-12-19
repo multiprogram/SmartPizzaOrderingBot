c69edae41353017125c5c88b06c8629914ab131e
multiprogram <126498341+multiprogram@users.noreply.github.com>
2021-12-19 14:02:11 +0000

    update ref continuing_education

--- pizzawit.py
+++ pizzawit.py
@@ -51,4 +51,4 @@ conv_place = 0
 conv_numb = 7
 conv = ['audio_johnnys_greeting.wav',
         'audio_johnnys_asking_for_toppings.wav',
-        'audio_johnnys_aski
+        'audio_johnnys_asking_is_that_al
