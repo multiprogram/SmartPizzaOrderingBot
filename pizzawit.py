9f69e9b5826dc0ad9ecb0efcdd20369bdbdfa07f
multiprogram <126498341+multiprogram@users.noreply.github.com>
2022-01-08 10:07:36 +0000

    Updated lockfile

--- pizzawit.py
+++ pizzawit.py
@@ -52,4 +52,4 @@ conv_numb = 7
 conv = ['audio_johnnys_greeting.wav',
         'audio_johnnys_asking_for_toppings.wav',
         'audio_johnnys_asking_is_that_all.wav',
-        'audio_johnnys
+        'audio_johnnys_asking_is_t
