dafbc19ca840f8eb5984d870edc7dc05f96b03c8
multiprogram <126498341+multiprogram@users.noreply.github.com>
2022-01-09 07:29:20 +0000

    Fix the README

--- pizzawit.py
+++ pizzawit.py
@@ -53,4 +53,4 @@ conv = ['audio_johnnys_greeting.wav',
         'audio_johnnys_asking_for_toppings.wav',
         'audio_johnnys_asking_is_that_all.wav',
         'audio_johnnys_asking_is_that_all.wav',
-  
+       
