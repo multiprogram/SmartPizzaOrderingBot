60f0d0916fa650e107f72ca6191e40c5761439f3
multiprogram <126498341+multiprogram@users.noreply.github.com>
2022-01-12 09:28:32 +0000

    ibmvscsi: Abort init sequence during error recovery

--- pizzawit.py
+++ pizzawit.py
@@ -53,4 +53,4 @@ conv = ['audio_johnnys_greeting.wav',
         'audio_johnnys_asking_for_toppings.wav',
         'audio_johnnys_asking_is_that_all.wav',
         'audio_johnnys_asking_is_that_all.wav',
-        'audio_johnnys_pickup_
+        'audio_johnnys_pickup_or_deliv
