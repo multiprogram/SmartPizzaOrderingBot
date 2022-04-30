e6cb04a5b7e781030d8feec86a37f085b87083b7
multiprogram <126498341+multiprogram@users.noreply.github.com>
2022-04-30 20:08:24 +0000

    new tutorial

--- pizzawit.py
+++ pizzawit.py
@@ -62,4 +62,4 @@ def get_phone_response():
     # store on the other end of the phone. We pretend and instead just return 
     # the name of the next wave file in our pretend conversation.
     #
- 
+    global 
