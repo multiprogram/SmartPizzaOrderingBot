99a443258819d85e5e8c8f6a05149eb0e651cf97
multiprogram <126498341+multiprogram@users.noreply.github.com>
2022-08-11 20:27:02 +0000

    init the repo

--- pizzawit.py
+++ pizzawit.py
@@ -85,4 +85,4 @@ def first_entity_intent_value(entities, entity):
 coke_ordered = False
 while True:
 
-    # Get the next thing said by the pizza plac
+    # Get the next thing said by the pizza place, have Wit
