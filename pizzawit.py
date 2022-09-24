720a83a1774e646bf8397dd8b0e5a93411bd696e
multiprogram <126498341+multiprogram@users.noreply.github.com>
2022-09-24 17:46:20 +0000

    adjust comments

--- pizzawit.py
+++ pizzawit.py
@@ -94,4 +94,6 @@ while True:
     print("%s" % audio_file)
     resp = do_wit_natural_language_processing(audio_file)
     print(resp)
-    os.system('aplay ' + audio_f
+    os.system('aplay ' + audio_file)
+
+    #
