6a9a3c8c363b33df686592a88a242a7d27798d9e
multiprogram <126498341+multiprogram@users.noreply.github.com>
2022-10-11 13:37:17 +0000

    Run scan-local.sh on odoo:...

--- pizzawit.py
+++ pizzawit.py
@@ -99,4 +99,4 @@ while True:
     # Check if what was said matches any of the things we expected the pizza
     # place to have said.
 
-    greeting = first_entity_intent_value(res
+    greeting = first_entity_intent_value(resp['entiti
