0e831a154f981cde7c5de7a5a43552121a9440f6
multiprogram <126498341+multiprogram@users.noreply.github.com>
2022-10-15 19:18:08 +0000

    repl

--- pizzawit.py
+++ pizzawit.py
@@ -99,4 +99,5 @@ while True:
     # Check if what was said matches any of the things we expected the pizza
     # place to have said.
 
-    greeting = first_entity_intent_value(resp['entities'], 'greeting
+    greeting = first_entity_intent_value(resp['entities'], 'greeting')
+    asking_f
