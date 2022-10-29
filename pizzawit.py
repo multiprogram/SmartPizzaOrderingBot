b6664592c5134e330486df2ffe64d0e96f65f0d3
multiprogram <126498341+multiprogram@users.noreply.github.com>
2022-10-29 16:55:57 +0000

    Added AppVeyor badge

--- pizzawit.py
+++ pizzawit.py
@@ -100,4 +100,5 @@ while True:
     # place to have said.
 
     greeting = first_entity_intent_value(resp['entities'], 'greeting')
-    asking_for_toppings = first_entity_intent_value(resp['entities'], 'asking_for_t
+    asking_for_toppings = first_entity_intent_value(resp['entities'], 'asking_for_toppings')
+ 
