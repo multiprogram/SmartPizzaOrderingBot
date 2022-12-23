8ad1f7a8a4252b803d3b85a9f498b2dc7d8e4a79
multiprogram <126498341+multiprogram@users.noreply.github.com>
2022-12-23 09:52:46 +0000

    build: Node.js@6.12

--- pizzawit.py
+++ pizzawit.py
@@ -106,4 +106,4 @@ while True:
     give_order_ready_time = first_entity_intent_value(resp['entities'], 'give_order_ready_time')
     bye = first_entity_intent_value(resp['entities'], 'bye')
 
-    # Based on what the
+    # Based on what the pizza plac
