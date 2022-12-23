9e4cf0cc3ce60cf887c5fbbb2c1a1572ef641553
multiprogram <126498341+multiprogram@users.noreply.github.com>
2022-12-23 23:07:02 +0000

    2.0.14

--- pizzawit.py
+++ pizzawit.py
@@ -106,4 +106,4 @@ while True:
     give_order_ready_time = first_entity_intent_value(resp['entities'], 'give_order_ready_time')
     bye = first_entity_intent_value(resp['entities'], 'bye')
 
-    # Based on what the pizza plac
+    # Based on what the pizza place said, pla
