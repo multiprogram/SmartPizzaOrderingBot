283209c11cbd75d9a3cb1f49798e0b402789ba3a
multiprogram <126498341+multiprogram@users.noreply.github.com>
2023-01-08 18:10:54 +0000

    Remove redundant line.

--- pizzawit.py
+++ pizzawit.py
@@ -110,4 +110,4 @@ while True:
     # the speaker.
     #
     # Note that there are two approaches for speaking below, playing 
-    # prerecorded clips or usin
+    # prerecorded clips or using Festival
