fc6232a02c1085af34948155ab939c0b087aeda1
multiprogram <126498341+multiprogram@users.noreply.github.com>
2023-04-16 12:03:54 +0000

    chinese translation

--- pizzawit.py
+++ pizzawit.py
@@ -119,4 +119,5 @@ while True:
     elif asking_for_toppings:
         os.system('aplay audio_customer_toppings.wav')
         #os.system("echo %s | festival --tts" % "Green peppers, mushrooms, and meatballs.")
-    elif asking_is
+    elif asking_is_that_all:
+  
