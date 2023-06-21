3a4a870ffa66468abb0d32a80c418b3bcb41df80
multiprogram <126498341+multiprogram@users.noreply.github.com>
2023-06-21 18:31:20 +0000

    Sync Develop branch

--- pizzawit.py
+++ pizzawit.py
@@ -126,4 +126,4 @@ while True:
             coke_ordered = True
         else:
             os.system('aplay audio_customer_yes.wav')
-            #os.system("echo %s | fest
+            #os.system("echo %s | festival --tts" %
