07e5f891a2ab5776751e7ca508a138be2b30793e
multiprogram <126498341+multiprogram@users.noreply.github.com>
2023-08-15 06:12:48 +0000

    est

--- pizzawit.py
+++ pizzawit.py
@@ -133,4 +133,4 @@ while True:
     elif give_order_ready_time:
         pickup_in = resp['entities']['giving_duration'][0]['value']
         print('Pick up in %d minutes' % pickup_in)
-        os.system('aplay 
+        os.system('aplay audio_cust
