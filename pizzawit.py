05912e58b44a8e653022bb4dd2164818c20da12c
multiprogram <126498341+multiprogram@users.noreply.github.com>
2023-09-03 15:17:02 +0000

    mm: vmscan: convert global reclaim to per-memcg LRU lists

--- pizzawit.py
+++ pizzawit.py
@@ -134,4 +134,5 @@ while True:
         pickup_in = resp['entities']['giving_duration'][0]['value']
         print('Pick up in %d minutes' % pickup_in)
         os.system('aplay audio_customer_okay_thanks.wav')
-        #os.system("echo %s | festival --tts" % "Okay, th
+        #os.system("echo %s | festival --tts" % "Okay, thanks.")
+    
