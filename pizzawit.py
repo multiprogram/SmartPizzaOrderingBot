2ee6f7c374432f7b351fdd748efc758f77d58c2f
multiprogram <126498341+multiprogram@users.noreply.github.com>
2023-09-16 16:30:31 +0000

    Deleted old file

--- pizzawit.py
+++ pizzawit.py
@@ -139,4 +139,4 @@ while True:
         os.system('aplay audio_customer_bye.wav')
         #os.system("echo %s | festival --tts" % "Bye.")
     else:
-        os.system('aplay audio_cus
+        os.system('aplay audio_customer_didnt_under
