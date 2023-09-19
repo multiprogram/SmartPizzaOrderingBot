f02f2991994259f3dedd05b9caa4aae39fce55fe
multiprogram <126498341+multiprogram@users.noreply.github.com>
2023-09-19 16:10:49 +0000

    Cleanup trailing whitespace

--- pizzawit.py
+++ pizzawit.py
@@ -139,4 +139,5 @@ while True:
         os.system('aplay audio_customer_bye.wav')
         #os.system("echo %s | festival --tts" % "Bye.")
     else:
-        os.system('aplay audio_customer_didnt_understand.wav'
+        os.system('aplay audio_customer_didnt_understand.wav')
+      
